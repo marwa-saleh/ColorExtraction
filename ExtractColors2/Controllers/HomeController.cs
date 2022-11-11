8a4966e2f7c966e10221509b1f4c4dedea63d451
Marwa Saleh <marwasaleh@microsoft.com>
2022-11-10 19:01:01 -0800

    add rest of colors

--- ExtractColors2/Controllers/HomeController.cs
+++ ExtractColors2/Controllers/HomeController.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using static System.Net.Mime.MediaTypeNames;
 using System.Drawing;
-//using SixLabors.ImageSharp;
-//using Image = SixLabors.ImageSharp.Image;
 using Color = System.Drawing.Color;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +11,7 @@ using Image = System.Drawing.Image;
 using SixLabors.ImageSharp;
 using System.Text;
 using Microsoft.Extensions.Primitives;
+using System;
 
 namespace ExtractColors2.Controllers
 {
@@ -34,73 +33,67 @@ namespace ExtractColors2.Controllers
         [HttpPost]
         public async Task<ActionResult> Index(IFormFile file)
         {
+            List<Colors> colors;
             using (var memoryStream = new MemoryStream())
             {
                 await file.CopyToAsync(memoryStream);
                 using (var image = Image.FromStream(memoryStream))
                 {
-                    ExtractColors(image);
+                    colors = ExtractColors(image);
+                    ImageConverter converter = new ImageConverter();
+                    ViewBag.Logo = (byte[])converter.ConvertTo(image, typeof(byte[]));
                 }
             }
                     
-            return View();
+            return View(colors);
         }
 
-        private void ExtractColors(Image image)
+        private List<Colors> ExtractColors(Image image)
         {
+            List<Colors> colors = new List<Colors>();
             Bitmap bitmap = new Bitmap(image);
 
             Color box1 = bitmap.GetPixel(209, 384);
-            int box1Blue = box1.B;
             int box1Saturation = (int)(box1.GetSaturation() * 100);
-            string box1Result = "Box 1: Saturation =" + box1Saturation + ", Blue =" + box1Blue; 
+            colors.Add(new Colors() { Name = "Right Bottom White", HSVS = box1Saturation, RGBB = box1.B, RGBR = box1.R, RGBG = box1.G });
 
             Color box2 = bitmap.GetPixel(215, 252);
-            int box2Blue = box2.B;
             int box2Saturation = (int)(box2.GetSaturation() * 100);
-            string box2Result = "Box 2: Saturation =" + box2Saturation + ", Blue =" + box2Blue;
+            colors.Add(new Colors() { Name = "Low", HSVS = box2Saturation, RGBB = box2.B, RGBR = box2.R, RGBG = box2.G });
 
             Color box3 = bitmap.GetPixel(211, 147);
-            int box3Blue = box3.B;
             int box3Saturation = (int)(box3.GetSaturation() * 100);
-            string box3Result = "Box 3: Saturation =" + box3Saturation + ", Blue =" + box3Blue;
+            colors.Add(new Colors() { Name = "Low Average", HSVS = box3Saturation, RGBB = box3.B, RGBR = box3.R, RGBG = box3.G });
 
             Color box4 = bitmap.GetPixel(217, 45);
-            int box4Blue = box4.B;
             int box4Saturation = (int)(box4.GetSaturation() * 100);
-            string box4Result = "Box 4: Saturation =" + box4Saturation + ", Blue =" + box4Blue;
+            colors.Add(new Colors() { Name = "Average", HSVS = box4Saturation, RGBB = box4.B, RGBR = box4.R, RGBG = box4.G });
 
             Color box5 = bitmap.GetPixel(135, 49);
-            int box5Blue = box5.B;
             int box5Saturation = (int)(box5.GetSaturation() * 100);
-            string box5Result = "Box 5: Saturation =" + box5Saturation + ", Blue =" + box5Blue;
+            colors.Add(new Colors() { Name = "Top White", HSVS = box5Saturation, RGBB = box5.B, RGBR = box5.R, RGBG = box5.G });
 
             Color box6 = bitmap.GetPixel(45, 51);
-            int box6Blue = box6.B;
             int box6Saturation = (int)(box6.GetSaturation() * 100);
-            string box6Result = "Box 6: Saturation =" + box6Saturation + ", Blue =" + box6Blue;
+            colors.Add(new Colors() { Name = "Above Average", HSVS = box6Saturation, RGBB = box6.B, RGBR = box6.R, RGBG = box6.G });
 
             Color box7 = bitmap.GetPixel(49, 174);
-            int box7Blue = box7.B;
             int box7Saturation = (int)(box7.GetSaturation() * 100);
-            string box7Result = "Box 7: Saturation =" + box7Saturation + ", Blue =" + box7Blue;
+            colors.Add(new Colors() { Name = "High", HSVS = box7Saturation, RGBB = box7.B, RGBR = box7.R, RGBG = box7.G });
 
             Color box8 = bitmap.GetPixel(46, 274);
-            int box8Blue = box8.B;
             int box8Saturation = (int)(box8.GetSaturation() * 100);
-            string box8Result = "Box 8: Saturation =" + box8Saturation + ", Blue =" + box8Blue;
+            colors.Add(new Colors() { Name = "Very High", HSVS = box8Saturation, RGBB = box8.B, RGBR = box8.R, RGBG = box8.G });
 
             Color box9 = bitmap.GetPixel(46, 372);
-            int box9Blue = box9.B;
             int box9Saturation = (int)(box9.GetSaturation() * 100);
-            string box9Result = "Box 9: Saturation =" + box9Saturation + ", Blue =" + box9Blue;
+            colors.Add(new Colors() { Name = "Left Bottom White", HSVS = box9Saturation, RGBB = box9.B, RGBR = box9.R, RGBG = box9.G });
 
             Color box10 = bitmap.GetPixel(127, 254);
-            int box10Blue = box10.B;
             int box10Saturation = (int)(box10.GetSaturation() * 100);
-            string box10Result = "Box 10: Saturation =" + box10Saturation + ", Blue =" + box10Blue;
+            colors.Add(new Colors() { Name = "Test Strip", HSVS = box10Saturation, RGBB = box10.B, RGBR = box10.R, RGBG = box10.G });
 
-            ViewBag.Message = new string[] { box1Result, box2Result, box3Result, box4Result, box5Result, box6Result, box7Result, box8Result, box9Result, box10Result };
+            return colors;
         }
         public IActionResult Privacy()
         {
---
+++ ExtractColors2/Models/Colors.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtractColors2.Models
+{
+    public class Colors
+    {
+            public string Name { get; set; }
+
+            public int HSVS { get; set; }
+
+            public int RGBB { get; set; }
+
+            public int RGBR { get; set; }
+
+            public int RGBG { get; set; }
+
+    }
+}
